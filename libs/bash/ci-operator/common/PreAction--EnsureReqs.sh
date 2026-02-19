f819f9b675dda351fa0fd01d90c36a27d6ba9ee0
Edoardo Tirtarahardja <54961875+etirta@users.noreply.github.com>
2026-02-19 09:58:12 -0500

    Common Library: PreAction-EnsureReqs and ExitTrap-PostProcessPrep. (#1)
    ## Related To
    - [[INTEROP-8866]](https://issues.redhat.com/browse/INTEROP-8866):
    Common Library: Create Exit Trap for CI Operator Step.
    
    ## Description
    ### Problem
    Some CI Operator Steps that execute the actual TCs may generate multiple
    jUnit XML TC Report files. Since we have to put them into
    `${SHARED_DIR}/` so the Data Router Step later can fetch them, this
    means there is a potential naming conflict, if there are multiple Steps
    in a Job generating these files. There is also a limitation on
    `${SHARED_DIR}/`, that it doesn't support sub-directories and has a
    limit of 1 MB.
    
    Also for Jobs that rely on CR (Component Readiness), they must have
    their TS Names match a certain pattern that is known to CR. This means
    there is a need to set the TS Names to ensure these requirements.
    
    The above post task is common across multiple LPs, so we need a
    maintainable solution. A library that can be sourced by multiple
    different Steps is very beneficial to achieve the desired solution.
    
    ### Solution
    A library function `PreAction-EnsureReqs()` is created to ensure all the
    required tooling is available, even if they are not built into the used
    Container Image.
    
    A library function `ExitTrap-PostProcessPrep()` is created to perform
    the required tasks before the Step exits.
    
    Using these library functions increases the maintainability and also
    guarantees consistency between multiple LP Interop Tests.

---
+++ libs/bash/ci-operator/common/PreAction--EnsureReqs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+function PreAction--EnsureReqs () {
+################################################################################
+#   Pre-Action Function that checks and installs the required tools.
+#
+#   Usage:
+#       eval "$(
+#           curl -fsSL \
+#       https://<urlAuthToRawContent>/<urlPathToRawContents...>\
+#       <repoPaths...>/PreAction--EnsureReqs.sh
+#       )"; PreAction--EnsureReqs <tools...>
+#
+#   Supported tools:
+#     - jq
+#     - yq
+################################################################################
+    typeset -a toolArr=("$@"); (($#)) && shift $#
+
+    export PATH="$(exec 3>&1 1>&2
+        typeset binDir="/tmp/bin" toolName=
+        mkdir -p "${binDir}"
+        while IFS= read -rd '' toolName; do
+            case ${toolName} in
+              (jq)
+                jq --version || {
+                    wget -qO "${binDir}/jq" \
+                        "https://github.com/jqlang/jq/releases/latest/download/jq-$(
+                            uname -s | tr '[:upper:]' '[:lower:]' | sed 's/darwin/macos/'
+                        )-$(
+                            uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/'
+                        )" &&
+                    chmod a+x "${binDir}/jq"
+                    "${binDir}/jq" --version
+                }
+                ;;
+              (yq)
+                yq --version || {
+                    wget -qO "${binDir}/yq" \
+                        "https://github.com/mikefarah/yq/releases/latest/download/yq_$(
+                            uname -s | tr '[:upper:]' '[:lower:]'
+                        )_$(
+                            uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/'
+                        )" &&
+                    chmod a+x "${binDir}/yq"
+                    "${binDir}/yq" --version
+                }
+                ;;
+              (*)   : "Unsupported tool: ${toolName}";;
+            esac
+        done 0< <(printf '%s\0' "${toolArr[@]}")
+        [[ ":${PATH}:" != *":${binDir}:"* ]] && echo "${binDir}:" 1>&3
+    )${PATH}"
+
+    return 0
+}
---
+++ libs/bash/ci-operator/interop/common/ExitTrap--PostProcessPrep.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+function ExitTrap--PostProcessPrep () {(
+################################################################################
+#   Exit Trap for CI Operator Step that executes the actual Test Cases.
+#
+#   CI Operator Step Script Trap Function to perform required actions necessary
+#   for post-processing.
+#
+#   Workflow:
+#    1. Collect and merge all jUnit XML Test Results from the executed Test
+#       Cases.
+#    2. If the Env. Var. `LP_IO__ET_PPP__NEW_TS_NAME` is set to non-empty, then
+#       the Test Suite Name is set to it.
+#    3. Archive the original jUnit XMLs into `${ARTIFACT_DIR}/`
+#    4. Put the merged jUnit XML into `${SHARED_DIR}/` for consumption of
+#       subsequent Step.
+#
+#   Usage:
+#       eval "$(
+#           curl -fsSL \
+#       https://<urlAuthToRawContent>/<urlPathToRawContents...>\
+#       <repoPaths...>/ExitTrap--PostProcessPrep.sh
+#       )"; trap '
+#           ExitTrap--PostProcessPrep junit--<unique-merge-filename>.xml
+#       ' EXIT
+#
+#   Required Env. Var. from Step Configuration:
+#       LP_IO__ET_PPP__NEW_TS_NAME
+#           The new Test Suite Name. Use `%s` as a placeholder for the
+#           original name (use `%%` for a literal `%`).
+#           This can be used to set the Test Suite Name to a specific
+#           searchable pattern, say for Component Readiness input
+#           filtering.
+#
+#   Used CI Operator default Env. Var.:
+#       ARTIFACT_DIR
+#       SHARED_DIR
+################################################################################
+    set -euxo pipefail; shopt -s inherit_errexit
+    typeset mergedFN="${1:-jUnit.xml}"; (($#)) && shift
+
+    typeset resultFile=''
+    typeset -a xmlFiles=()
+
+    # Ensure requirements are met.
+    eval "$(
+        curl -fsSL \
+https://raw.githubusercontent.com/RedHatQE/OpenShift-LP-QE--Tools/refs/heads/main/\
+libs/bash/ci-operator/common/PreAction--EnsureReqs.sh
+    )"; PreAction--EnsureReqs yq
+
+    while IFS= read -r -d '' resultFile; do
+        grep -qE '<testsuites?\b' "${resultFile}" && xmlFiles+=("${resultFile}")
+    done 0< <(
+        find "${ARTIFACT_DIR}/" \
+            -type f -iname "*.xml" ! -name "${mergedFN}" \
+            -print0
+    )
+
+    ((${#xmlFiles[@]})) || {
+        : 'WARNING: No jUnit XML file found to process!'
+        return
+    }
+
+    # Collect all jUnit XMLs, set TS Name if applicable, and merge them to one.
+    yq eval-all -px -ox -I2 '
+        [.] | {
+            "+p_xml": "version=\"1.0\" encoding=\"UTF-8\"",
+            "testsuites": {"testsuite": [
+                .[][] |
+                select(kind == "map") |
+                (.testsuite // .) |
+                ([] + .)[] | (
+                    select(strenv(LP_IO__ET_PPP__NEW_TS_NAME) != "") |
+                    (."+@name" // "") as $oldName |
+                    ."+@name" = (
+                        strenv(LP_IO__ET_PPP__NEW_TS_NAME) |
+                        sub("(^|[^%])((%%)*)%s", "${1}${2}\($oldName)") |
+                        sub("%%", "%")
+                    )
+                )//. |
+                ([] + (.testcase // [])) as $tc |
+                ."+@tests" = ($tc | length | tostring) |
+                ."+@failures" = ([$tc[] | select(.failure)] | length | tostring) |
+                ."+@errors" = ([$tc[] | select(.error)] | length | tostring)
+            ]}
+        }
+    ' "${xmlFiles[@]}" 1> "${ARTIFACT_DIR}/${mergedFN}"
+
+    # Archive the original jUnit XMLs.
+    {
+        tar \
+            zcf "${ARTIFACT_DIR}/jUnit-original.tgz" \
+            -C "${ARTIFACT_DIR}/" \
+            "${xmlFiles[@]#${ARTIFACT_DIR}/}" &&
+        rm -f "${xmlFiles[@]}"
+    }
+
+    # The Data Router Step needs to be able to fetch the merged jUnit XML.
+    cp "${ARTIFACT_DIR}/${mergedFN}" "${SHARED_DIR}/"
+
+    true
+)}
